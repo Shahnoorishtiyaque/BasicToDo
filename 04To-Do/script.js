8b6f5f256ce1c59b5ebb9636c7d25069b48dfa5e
SHAHNOOR ISHTIYAQUE <148809310+Shahnoorishtiyaque@users.noreply.github.com>
2025-06-17 16:58:05 +0530

    Update script.js

--- 04To-Do/script.js
+++ 04To-Do/script.js
@@ -5,50 +5,50 @@ AddWork.addEventListener("click",function(){
 console.log(TaskList);
         
         let taskInput=document.getElementById("Input");
-        if(taskInput.value.trim()==""){  //trim() is use to remove extra space from starting and end
+        if(taskInput.value.trim()==""){ 
             alert("Add some task")
             return;
         }
         
-        //create a list
+       
         let NewList=document.createElement("li")
-        //add in ul
+        
         let List=TaskList.appendChild(NewList)
 
-         //create a div
+         
          let NewDiv=document.createElement("div")
          NewDiv.classList.add("description")
-        //  add in li
+        
         let Div=List.appendChild(NewDiv)
         
-        //create a img
+        
         let NewImg=document.createElement("img")
         NewImg.classList.add("Checkbox")
         NewImg.src="circle.png"
 
-        //add it to div
+        
         Div.appendChild(NewImg)
-        //create a p
+        
         let para=document.createElement("p")
         para.classList.add("Task")
-        //add TaskInput to para
+        
         para.textContent=taskInput.value
-         //add it to Div
+        
          Div.appendChild(para)
 
-         //create a delete button
+         
          let deleteButton=document.createElement("button")
          deleteButton.classList.add("delete")
          deleteButton.textContent="DELETE"
 
-         //add it to List
+
 
         List.appendChild(deleteButton)
         
         taskInput.value="";
 
 
-        //give click functionality to checkbox
+        
         NewImg.addEventListener("click",function(){
            if (NewImg.src.includes("circle.png")) {
              console.log("Clicked");
@@ -59,7 +59,7 @@ console.log(TaskList);
             para.classList.remove("cut")
            }
         })
-        //give  click function to detele button
+        
         deleteButton.addEventListener("click",function(){
             deleteButton.parentElement.remove()
         })
